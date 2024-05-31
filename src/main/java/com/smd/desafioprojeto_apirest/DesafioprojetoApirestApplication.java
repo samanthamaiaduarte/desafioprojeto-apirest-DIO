b25091a834d1d80c221a1a7228869e60eb430c64
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2024-05-31 16:32:43 -0300

    Initializing project

---
+++ src/main/java/com/smd/desafioprojeto_apirest/DesafioprojetoApirestApplication.java
@@ -0,0 +1,13 @@
+package com.smd.desafioprojeto_apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioprojetoApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioprojetoApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smd/desafioprojeto_apirest/DesafioprojetoApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smd.desafioprojeto_apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioprojetoApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
